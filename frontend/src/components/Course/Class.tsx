83a01610caa21d80fb3506f87d957f701290c17d
CatherineZhuang <catherinezhuzhuang@gmail.com>
2020-02-14 13:36:24 -0500

    initial try

--- frontend/src/components/Course/Class.tsx
+++ frontend/src/components/Course/Class.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 interface ClassProps {
+    id: string;
     courseName: string;
     percent: number;
     color: string;
@@ -11,13 +13,18 @@ interface ClassState {
 }
 
 class Class extends React.Component<ClassProps, ClassState> {
+    toCoursePage(id: string) {
+        <Redirect to={`/course/${id}`} />
+    }
+
     render() {
         return (
             <div className="class">
                 <div id="courseName">{this.props.courseName}</div>
-                <div id="percentage" style={{backgroundColor: this.props.color}}>
+                <div id="percentage" style={{ backgroundColor: this.props.color }}>
                     {this.props.percent + "%"}
                 </div>
+                <button onClick={() => this.toCoursePage(this.props.id)}></button>
             </div>
         );
     }
