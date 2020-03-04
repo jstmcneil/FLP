144bca9c9d1c7b23b3331d4ce72eb9d100d913d5
Aditya Nadkarni <anadkarni@lawn-143-215-112-141.lawn.gatech.edu>
2020-03-04 17:21:04 -0500

    show correct profile and add a logout button

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Login from '../Login';
 import Register from '../Register';
 import StudentProfile from './StudentProfile';
 import InstructorProfile from './InstructorProfile';
+import { connect } from 'react-redux';
+import { loggedInSelector, isInstructorSelector } from '../../selectors';
 
 
-class Profile extends React.Component {
+interface ProfileProps {
+    loggedIn: boolean;
+    isInstructor: boolean;
+}
+
+class Profile extends React.Component<ProfileProps> {
+
+    renderProfile(loggedIn: boolean, isInstructor: boolean) {
+        if (!loggedIn) {
+            return <Fragment />;
+        }
+        if (isInstructor) {
+            return <Fragment><div className="title">My Profile</div><InstructorProfile /></Fragment>;
+        }
+        return <Fragment><div className="title">My Profile</div><StudentProfile /></Fragment>
+    }
+
     render() {
         return (
             // @ts-ignore
             <div style={{ "display": "flex", "flex-direction": "column" }}>
-                <div className="title">My Profile</div>
-                <Login />
-                <Register />
-                <StudentProfile />
-                <InstructorProfile />
+                {!this.props.loggedIn && <Login /> }
+                {!this.props.loggedIn && <Register /> }
+                {this.renderProfile(this.props.loggedIn, this.props.isInstructor)}
             </div>
         );
     }
 }
 
-export default Profile;
+export default connect(state => {
+    return {
+        loggedIn: loggedInSelector(state),
+        isInstructor: isInstructorSelector(state)
+    }
+})(Profile);
