8feae9e7d5ebbf984a72d3817c64cb2028b735eb
CatherineZhuang <catherinezhuzhuang@gmail.com>
2020-03-06 16:46:47 -0500

    revise course and course detail page

---
+++ frontend/src/model/EmailType.tsx
@@ -0,0 +1,5 @@
+export interface EmailType {
+    questionContent: string;
+    videoType: string;
+    videoId: string;
+}
--- frontend/src/selectors/index.ts
+++ frontend/src/selectors/index.ts
@@ -1,4 +1,6 @@
+import { CourseType } from '../model/CourseType';
 export const loggedInSelector = (state: any): boolean => state.loggedIn;
 export const accountIdSelector = (state: any): string => state.accountId;
 export const isInstructorSelector = (state: any): boolean => state.isInstructor;
 export const usernameSelector = (state: any): string => state.username;
+export const courseSelector = (state: any): CourseType[] => state.courses;     
