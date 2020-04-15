036954215e69f98951ec1d240d866669cdd0e211
Aditya Nadkarni <anadkarni@Adityas-MacBook-Air.local>
2020-04-14 23:47:01 -0400

    updated home page styles

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 import logo from '../../img/buzz.png';
 import Login from '../Login';
 import VideoPlayer from '../VideoPlayer';
+import { Typography } from '@material-ui/core';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                <div className="title">Home</div>
-                <img src={logo} alt="buzz" />
-                <h2>Welcome to Georgia Tech Financial Learning Curriculum</h2>
+            <div style={{width:"100%", height:"500px"}}>
+                <AutoSizer>
+                    {({ width }) => {
+                        return (
+                            <div className="verticalContainer" style={{ width, textAlign: "center" }}>
+                                <Typography component={"div"} variant="h2" style={{width: "100%"}}>Welcome to Georgia Tech Financial Learning Curriculum</Typography>
+                                <div style={{display: "block", marginRight: "auto", marginLeft: "auto"}}>
+                                    <img src={logo} alt="buzz" style={{maxWidth: "800px"}}/>
+                                </div>
+                                
+                            </div>
+                        )
+                    }
+                    }
+                </AutoSizer>
+
+
             </div>
         );
     }
